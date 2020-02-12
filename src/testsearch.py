b8d2cba2b5d3eae7b085f1ae933c7903b9d034f0
joshmcintosh <46505027+joshmcintosh@users.noreply.github.com>
2020-02-12 15:05:11 -0800

    Update testsearch.py

--- src/testsearch.py
+++ src/testsearch.py
@@ -12,11 +12,13 @@ import argparse
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+from getdevkey import getdevkey
+
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyCc9H2yJYj9kvRXglyXsAk9M1l6TGOfaT4"
+DEVELOPER_KEY = getdevkey()
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
