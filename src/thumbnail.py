1fabcb3a0e265481bbb91cc0d7d0577444b76008
terahn <terahnharrison@gmail.com>
2020-03-07 14:11:41 -0800

    various improvements for fetching thumbnails

--- src/thumbnail.py
+++ src/thumbnail.py
@@ -7,21 +7,30 @@ import time
 import numpy as np
 from dataset import load_dataset
 
+EMPTY_THUMBNAIL = np.load('../data/empty_thumbnail.npy') # the thumbnail that is shown when a video has been deleted/removed
+
 def get_thumbnails(video_ids):
     thumbnails = []
     missing = []
     count = 0
     for video_id in video_ids:
-        url = 'https://i.ytimg.com/vi/' + video_id + '/default.jpg'
-        print('Loading thumbnail for video {0} ({1})'.format(video_id, count))
         # load image from url
         try:
+            print('Loading thumbnail for video {0} ({1})'.format(video_id, count))
+        
+            url = 'https://i.ytimg.com/vi/' + video_id + '/default.jpg'
+
             response = requests.get(url)
             img = Image.open(BytesIO(response.content))
 
             # convert to numpy array
             np_img = np.array(img)
-            thumbnails.append(np_img)
+
+            # check to see if thumbnail is empty
+            if np.array_equal(np_img, EMPTY_THUMBNAIL):
+                thumbnails.append(-1)
+            else:
+                thumbnails.append((video_id, np_img))
         except:
             print('Error retrieving thumbnail for: ', video_id)
             thumbnails.append(-1)
@@ -32,7 +41,7 @@ def get_thumbnails(video_ids):
 def main():
 
     # load video_ids from dataset
-    path_to_dataset = '../data/CAvideos.csv'
+    path_to_dataset = '../data/custom_dataset.csv'
     dataset = load_dataset(path_to_dataset, ['video_id'])
 
     video_ids = dataset['video_id'].tolist()
@@ -43,10 +52,13 @@ def main():
     end = time.time()
     print('Completed in: ', end - start)
 
-    # save to NPY file
-    np.save('thumbnails.npy', thumbnails)
+    # remove any empty values
+    filtered = thumbnails[thumbnails != -1]
 
-    print('thumbnails.shape: ', thumbnails.shape)
+    # save to NPY file
+    np.save('thumbnails.npy', filtered)
+
+    print('Output shape: ', filtered.shape)
 
 
 
